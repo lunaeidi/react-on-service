cabcdbaf346ef5cb9db079d89ff313cf0a2bf855
Nicole Eidi <nicoleeidi1@gmail.com>
2018-12-29 19:33:14 -0800

    signup-bundle

---
+++ app/javascript/packs/signup-bundle.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Signup from '../bundles/HelloWorld/components/Signup';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  Signup,
+});
