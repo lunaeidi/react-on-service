291c7eb6919cab87a9220d512fdf59c7bf04d266
Nicole Eidi <nicoleeidi1@gmail.com>
2019-01-03 17:44:37 -0800

    address validation

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.3'
-gem 'react_on_rails', '11.1.7'    
+ruby '2.3.1'
+gem 'react_on_rails', '11.1.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
--- app/javascript/bundles/HelloWorld/components/Signup.jsx
+++ app/javascript/bundles/HelloWorld/components/Signup.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 class Signup extends Component {
   state={
     username: "",
-    password:""
+    password:"",
+    address1:"",
+    address2:"",
+    city:"",
+    state:"",
+    zip:""
   }
   handleUsername(event) {
   this.setState({
@@ -14,7 +19,26 @@ handlePassword(event){
     password: event.target.value
   })
 }
+handleAddress1(event){
+  this.setState({
+    address1: event.target.value
+  })
+}
+validate(){
+
+
+fetch('http://production.shippingapis.com/ShippingAPITest.dll?API=Verify&XML=<AddressValidateRequest USERID="949FLATI6234"><Address ID="0"><Address1></Address1><Address2>6406 Ivy Lane</Address2><City>Greenbelt</City><State>MD</State><Zip5></Zip5><Zip4></Zip4></Address></AddressValidateRequest>')
+  .then(response => response.text())
+          .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+          .then(data => {
+            console.log(data)
+            
+          })
+}
 handleSubmit(event){
+  if (validate != "false"){
+
+  }else{
     let postBody= {
     username: this.state.username,
     password: this.state.password,
@@ -32,6 +56,7 @@ handleSubmit(event){
               }
           }).then(res => res.json())
                             .then(res => console.log(res))
+              }
   }
 
   render() {
@@ -40,7 +65,7 @@ handleSubmit(event){
         <form>
             <p>Username:<input value={this.state.username}onChange={(event) => this.handleUsername(event)}type="text" /></p>
             <p>Password:<input value={this.state.password}onChange={(event) => this.handlePassword(event)}type="password"/></p>
-
+            <p>Address1:<input value={this.state.address1}onChange={event => this.handleAddress1(event)}type="text"></input></p>
           <input type="button" value="Signup"  onClick={ event => this.handleSubmit(event) }/>
         </form>
       </div>
